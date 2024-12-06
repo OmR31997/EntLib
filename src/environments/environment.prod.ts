7094a93f3efb6fd0b9be44f21916e27ff6eb0c5d
OmR31997 <omr31997@gmail.com>
2024-12-06 23:04:35 +0530

    Deploy to GitHub Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: '',  // Replace during build
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
+    apiKey: '__API_KEY__',  // Replace during build
+    authDomain: '__AUTH_DOMAIN__',
+    databaseURL: '__DATABASE_URL__',
+    projectId: '__PROJECT_ID__',
+    storageBucket: '__STORAGE_BUCKET__',
+    messagingSenderId: '__MSG_SENDER_ID__',
+    appId: '__APP_ID__',
+    measurementId: '__MEASUREMENT_ID__'
   },
   entertaiment: {
     movieApiUrl:'',
